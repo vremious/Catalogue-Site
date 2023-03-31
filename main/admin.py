aaf2e602796dd9b2e05e30d87fc18928e694bfc7
Denis <vremious@gmail.com>
2023-03-31 16:51:05 +0300

    -some changes on html tempates

--- main/admin.py
+++ main/admin.py
@@ -7,9 +7,9 @@ from django.contrib.auth.models import User
 from django.contrib.admin.models import LogEntry
 
 class LogEntryAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'action_time', 'user', 'content_type', 'object_repr')
-    list_filter = ('content_type','user__username')
-    search_fields = ['user__username']
+    list_display = ('__str__', 'action_time' ,'user', 'object_repr')
+    list_filter = ('user__username',)
+    search_fields = ['object_repr']
     date_hierarchy = 'action_time'
 
 admin.site.register(LogEntry, LogEntryAdmin)
