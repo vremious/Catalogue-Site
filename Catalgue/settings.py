bc5917299e53e9ae21a4f1ff15116de2728f29b1
Denis Emelyanov <101509271+vremious@users.noreply.github.com>
2023-11-03 17:50:10 +0300

    Update settings.py

--- Catalgue/settings.py
+++ Catalgue/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f)^b*pb0fyzdpb5ma&yq!di44*_3=us^frue_)ehyut10@w72y'
+SECRET_KEY = 'test'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
